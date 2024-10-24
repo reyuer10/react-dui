5a6b0015c567f31f6dd5465e368c2a762210a184
reyuer <reyuer10@gmail.com>
2024-10-24 08:31:14 +0800

    Finish the table list and revise some little design

--- react-dui-main/src/pages/tables/TableList.jsx
+++ react-dui-main/src/pages/tables/TableList.jsx
@@ -35,6 +35,10 @@ function TableList() {
     navigate("/");
   };
 
+  const handleSelectTable = () => {
+    navigate("/color-game/select-view");
+  };
+
   return (
     <div className="min-h-screen font-rubik flex flex-col items-center justify-center bg-gradient-to-t from-gray-700 via-amber-600 to-amber-400">
       <div className="absolute left-0 top-0 p-5">
@@ -43,29 +47,55 @@ function TableList() {
         </button>
       </div>
       <div>
-        <p className="text-[42px] font-black m-8">Select Table</p>
+        <p className="text-[42px] font-black m-8 text-white primary-drop-shadow">
+          Select Table
+        </p>
       </div>
       <div className="border-[6px] border-black rounded-3xl w-[60%]">
         <table className="w-full ">
           <thead>
             <tr>
-              <th className="px-4 border-b border-black ">Name</th>
-              <th className="px-4 border-b border-l border-black">
+              <th className="px-4 border-b border-black text-white">Name</th>
+              <th className="px-4 border-b border-l border-black text-white">
                 Date Created
               </th>
-              <th className="px-4 border-b border-l border-black">Status</th>
-              <th className="px-4 border-b border-l border-black">Action</th>
+              <th className="px-4 border-b border-l border-black text-white">
+                Status
+              </th>
+              <th className="px-4 border-b border-l border-black text-white">
+                Action
+              </th>
             </tr>
           </thead>
-          <tbody className="m-6">
+          <tbody className="m-6 ">
             {dataTables.map((d) => {
               return (
                 <tr key={d.id} className=" border-t border-black">
-                  <td className="text-center py-2 font-black">{d.name}</td>
-                  <td className="text-center">{d.dateCreated}</td>
-                  <td className="text-center">{d.status}</td>
+                  <td className="text-center py-2 font-black border-r border-black text-white">
+                    {d.name}
+                  </td>
+                  <td className="text-center border-r border-black text-white">
+                    {d.dateCreated}
+                  </td>
+                  <td className="text-center font-bold border-r border-black text-white">
+                    <button
+                      className={`${
+                        d.status === "Available" ? "bg-green-500" : "bg-red-500"
+                      } font-bold bg-black text-slate-200 px-4 rounded`}
+                    >
+                      {d.status}
+                    </button>
+                  </td>
                   <td className="text-center">
-                    <button className="font-bold">Open</button>
+                    <button
+                      onClick={handleSelectTable}
+                      disabled={d.status === "Playing"}
+                      className={`${
+                        d.status === "Available" ? "" : " opacity-50"
+                      } font-bold bg-black text-slate-200 px-4 rounded`}
+                    >
+                      Open
+                    </button>
                   </td>
                 </tr>
               );
--- react-dui-main/src/pages/view/SelectView.jsx
+++ react-dui-main/src/pages/view/SelectView.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function SelectView() {
   const navigate = useNavigate();
   const handleChangeTable = () => {
-    navigate("/");
+    navigate("/color-game/select-table");
   };
 
   const handleRouteTrendDisplay = () => {
@@ -18,12 +18,17 @@ function SelectView() {
 
   return (
     <div className="min-h-screen box-border flex flex-col font-rubik items-center justify-center bg-gradient-to-t from-gray-700 via-amber-600 to-amber-400">
+      <div className="absolute left-0 top-0 p-5">
+        <button onClick={handleChangeTable} className="font-bold text-orange-700">
+          BACK
+        </button>
+      </div>
       <div className="p-4 space-y-14 w-[500px]">
         <div className="flex justify-center">
           <img src={colorGameLogo} alt="casino-logo" className="w-[80%]" />
         </div>
         <div className="text-center flex items-center p-2 justify-evenly text-3xl font-black text-black border-4 border-black shadow shadow-black rounded-full">
-          <p className="">Table Name:</p>
+          <p className="text-white primary-drop-shadow">Table Name:</p>
           <p className="bg-black rounded-full px-5 py-1 text-white">CJ01</p>
         </div>
         <div className="space-y-6 bg-zinc-700 border-[6px] border-black p-6 rounded-3xl">
@@ -44,15 +49,15 @@ function SelectView() {
               Dealer
             </button>
           </div>
-          <div className="text-center text-xl space-y-4">
+          {/* <div className="text-center text-xl space-y-4">
             <p className="font-black text-white">Change Table</p>
             <button
               onClick={handleChangeTable}
               className=" bg-yellow-300 shadow-md shadow-black border-6 w-full rounded-full py-2 font-black text-[18px] text-orange-700 transition-colors hover:bg-yellow-200"
             >
-              Logout
+              Back
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
